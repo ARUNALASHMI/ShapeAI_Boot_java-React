2c485b8c3c4fa535cea38dd1c972219d7b67df49
ARUNALASHMI <85407156+ARUNALASHMI@users.noreply.github.com>
2021-06-05 22:42:01 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>ROYAL ENFIELD</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Royal enfied models</h1>
+      <p1>
+        1.Royal Enfield Himalayan.. 2.Royal Enfield Classic 350.. 3.Royal
+        Enfield Bullet 500..
+      </p1>
+    </div>
+  );
+}
+
+export default Note;
